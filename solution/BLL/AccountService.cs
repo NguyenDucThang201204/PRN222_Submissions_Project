f5da51c514b656749f579f1a106ed590663795b5
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:18 +0700

    Upload solution/BLL/AccountService.cs

---
+++ solution/BLL/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+using DAL;
+
+namespace BLL
+{
+    public class AccountService : IAccountService
+    {
+        private IAccountRepo repo;
+        public AccountService(IAccountRepo repo)
+        {
+            this.repo = repo;
+        }
+        public PantherAccount? Login(string email, string password)
+        {
+            return repo.GetPantherAccount(email, password);
+        }
+    }
+}
