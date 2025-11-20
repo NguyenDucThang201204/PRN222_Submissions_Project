3dea54068134cd41ed062c1fda5b7b5bcea362ef
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:40 +0700

    Upload solution/PantherPetManagement.Repository/IPantherProfileRepo.cs

---
+++ solution/PantherPetManagement.Repository/IPantherProfileRepo.cs
@@ -0,0 +1,23 @@
+﻿using PantherPetManagement.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantherPetManagement.Repository
+{
+    public interface IPantherProfileRepo
+    {
+
+        void SavePanther(PantherProfile p);
+
+        void DeletePanther(PantherProfile p);
+
+        void UpdatePanther(PantherProfile p);
+
+        List<PantherProfile> GetPanther();
+
+        PantherProfile GetPantherById(int id);
+    }
+}
