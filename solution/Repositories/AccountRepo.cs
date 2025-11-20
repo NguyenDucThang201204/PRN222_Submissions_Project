eb724ca8a2b1bdbce8b1458c273a86c73f14fe04
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:29:02 +0700

    Upload solution/Repositories/AccountRepo.cs

---
+++ solution/Repositories/AccountRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public  class AccountRepo
+    {
+        public PantherAccount LoginAccount(string Email, string password)
+        {
+            using (var context = new SU25PantherDBContext())
+            {
+                var account = context.PantherAccounts
+                    .FirstOrDefault(a => a.Email == Email && a.Password == password);
+
+                if (account != null)
+                {
+                    return account;
+                }
+                return null;
+            }
+        }
+    }
+}
