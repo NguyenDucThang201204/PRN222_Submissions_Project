fbc0b40d99fdfd5b3d8440f61bcc45e3f169928b
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:11 +0700

    Upload solution/PantherPetManagement.DAL/Repositories/IPantherAccountRepository.cs

---
+++ solution/PantherPetManagement.DAL/Repositories/IPantherAccountRepository.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PantherPetManagement.DAL.Repositories
+{
+    public interface IPantherAccountRepository
+    {
+        Task<PantherAccount> Login(string email, string password);
+    }
+}
