1147a6dc7c55f9c55840f56ddb5e53ad0a301c2d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:09 +0700

    Upload solution/PantherPetManagement.BLL/Services/PantherAccountService.cs

---
+++ solution/PantherPetManagement.BLL/Services/PantherAccountService.cs
@@ -0,0 +1,20 @@
+﻿using PantherPetManagement.DAL;
+using PantherPetManagement.DAL.Repositories;
+
+
+namespace PantherPetManagement.BLL.Services
+{
+    public class PantherAccountService : IPantherAccountService
+    {
+        private readonly IPantherAccountRepository _repo;
+        public PantherAccountService(IPantherAccountRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<PantherAccount> Login(string email, string password)
+        {
+            return await _repo.Login(email, password);
+        }
+
+    }
+}
