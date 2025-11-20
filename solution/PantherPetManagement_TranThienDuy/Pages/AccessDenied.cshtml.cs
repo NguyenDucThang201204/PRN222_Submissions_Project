95ec6b0c9c280e8dfd0c85f3568ba811333d5433
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:29:11 +0700

    Upload solution/PantherPetManagement_TranThienDuy/Pages/AccessDenied.cshtml.cs

---
+++ solution/PantherPetManagement_TranThienDuy/Pages/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PantherPetManagement_TranThienDuy.Pages
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
