e12a3836611cb7ad788b66a29d80a302f923e3b6
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:12 +0700

    Upload solution/PantherPetManagement.DAL/Repositories/IPantherProfileRepository.cs

---
+++ solution/PantherPetManagement.DAL/Repositories/IPantherProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace PantherPetManagement.DAL.Repositories
+{
+    public interface IPantherProfileRepository
+    {
+        Task<List<PantherProfile>> GetAllAsync();
+        Task<List<PantherProfile>> GetAllByOr(double? search1, string search2);
+        Task AddAsync(PantherProfile add);
+        Task DeleteAsync(int id);
+        Task UpdateASync(PantherProfile update);
+        Task<PantherProfile> GetByIdAsync(int id);
+    }
+}
