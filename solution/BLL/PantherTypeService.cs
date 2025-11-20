546c87d385f8b8f2fcb6403f53f3f539d88b393d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:23 +0700

    Upload solution/BLL/PantherTypeService.cs

---
+++ solution/BLL/PantherTypeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+using DAL;
+
+namespace BLL
+{
+    public class PantherTypeService : IPantherTypeService
+    {
+        private readonly IPantherTypeRepo _repo;
+        public PantherTypeService(IPantherTypeRepo repo)
+        {
+            _repo = repo;
+        }
+        public List<PantherType> GetPantherTypes()
+        {
+            return _repo.GetPantherTypes();
+        }
+    }
+}
