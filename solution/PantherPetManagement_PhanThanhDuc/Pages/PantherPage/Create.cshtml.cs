cb6e99de791a44332f335d0f3cb17a39c78e411d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:26:43 +0700

    Upload solution/PantherPetManagement_PhanThanhDuc/Pages/PantherPage/Create.cshtml.cs

---
+++ solution/PantherPetManagement_PhanThanhDuc/Pages/PantherPage/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PantherPetManagement_PhanThanhDuc.Pages.PantherPage
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
