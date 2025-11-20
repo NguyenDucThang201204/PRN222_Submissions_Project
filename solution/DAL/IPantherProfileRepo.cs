0b06adabd3f21cb18bc476ff8f7bc59212cd71a8
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:30 +0700

    Upload solution/DAL/IPantherProfileRepo.cs

---
+++ solution/DAL/IPantherProfileRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace DAL
+{
+    public interface IPantherProfileRepo
+    {
+        List<PantherProfile> GetAll();
+        PantherProfile? GetById(int id);
+        void Add(PantherProfile profile);
+        void Update(PantherProfile profile);
+        void Delete(int id);
+        List<PantherProfile> Search(double? weight, string pantherTypeName);
+    }
+}
