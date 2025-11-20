0f0a6475f8e16d01eb269da5896ac55683f1bb93
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:47 +0700

    Upload solution/PantherPetManagement.Service/PantherAccountService.cs

---
+++ solution/PantherPetManagement.Service/PantherAccountService.cs
@@ -0,0 +1,22 @@
+﻿using PantherPetManagement.Repository;
+using PantherPetManagement.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantherPetManagement.Service
+{
+    public class PantherAccountService : IPantherAccountService
+    {
+        private readonly IPantherAccountRepo _repo;
+        public PantherAccountService(IPantherAccountRepo repo) {
+        _repo = repo;
+        }
+        public PantherAccount GetAccountById(int id)
+        {
+            return _repo.GetPantherAccountById(id);
+        }
+    }
+}
