97f2055a6554c6008196eb9e5188317cceb2b890
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:26:58 +0700

    Upload solution/PantherPetManagement.DAL/PantherProfile.cs

---
+++ solution/PantherPetManagement.DAL/PantherProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace PantherPetManagement.DAL;
+
+public partial class PantherProfile
+{
+    public int PantherProfileId { get; set; }
+
+    public int PantherTypeId { get; set; }
+
+    public string PantherName { get; set; } = null!;
+
+    public double Weight { get; set; }
+
+    public string Characteristics { get; set; } = null!;
+
+    public string Warning { get; set; } = null!;
+
+    public DateTime ModifiedDate { get; set; }
+
+    public virtual PantherType? PantherType { get; set; } = null!;
+}
