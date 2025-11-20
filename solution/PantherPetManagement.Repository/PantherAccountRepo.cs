705ac28289b2bd9c4e162ee4547700618cf088f2
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:41 +0700

    Upload solution/PantherPetManagement.Repository/PantherAccountRepo.cs

---
+++ solution/PantherPetManagement.Repository/PantherAccountRepo.cs
@@ -0,0 +1,18 @@
+﻿using PantherPetManagement.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantherPetManagement.Repository
+{
+    public class PantherAccountRepo : IPantherAccountRepo
+    {
+        public PantherAccount GetPantherAccountById(int id)
+        {
+            using var context = new Su25pantherDbContext();
+            return context.PantherAccounts.FirstOrDefault(x => x.AccountId.Equals(id));
+        }
+    }
+}
