518c237da3f13b899de9437a0161b7f496c3cd7d
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:45 +0700

    Upload solution/PantherPetManagement.Service/IPantherTypeService.cs

---
+++ solution/PantherPetManagement.Service/IPantherTypeService.cs
@@ -0,0 +1,14 @@
+﻿using PantherPetManagement.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantherPetManagement.Service
+{
+    public interface IPantherTypeService
+    {
+        List<PantherType> GetCategories();
+    }
+}
