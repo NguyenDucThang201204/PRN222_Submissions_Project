cddbb8b6df8f988e5d2d95313e08e20f9905bd29
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:37 +0700

    Upload solution/PantherPetManagement_NguyenThaiDuy/Pages/BasePageModel.cs

---
+++ solution/PantherPetManagement_NguyenThaiDuy/Pages/BasePageModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace PantherPetManagement_NguyenThaiDuy.Pages
+{
+    public class BasePageModel : PageModel
+    {
+        // biến kiểm tra đăng nhập chưa
+        protected bool IsLoggedIn => HttpContext.Session.GetInt32("UserId") != null;
+        public bool IsManager => HttpContext.Session.GetInt32("RoleId") == 2; // Manager role
+        protected bool IsStaff => HttpContext.Session.GetInt32("RoleId") == 3; // Staff role
+
+        public string ErrorMessage { get; set; }
+
+        protected IActionResult CheckPermission(bool requireManager = false)
+        {
+            if (!IsLoggedIn)
+            {
+                return RedirectToPage("/Login");
+            }
+
+            if (requireManager && !IsManager)
+            {
+                ErrorMessage = "You have no permission to access this function!";
+                return RedirectToPage("/PantherProfile/Index", new { error = ErrorMessage });
+            }
+
+            return null;
+        }
+
+        // Add logout functionality
+        protected IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("/Login");
+        }
+    }
+}
