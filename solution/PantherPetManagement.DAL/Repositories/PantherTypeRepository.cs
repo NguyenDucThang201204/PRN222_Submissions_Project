05b26935f61d42dcf7f569a9c2bd20266bb6dc14
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:16 +0700

    Upload solution/PantherPetManagement.DAL/Repositories/PantherTypeRepository.cs

---
+++ solution/PantherPetManagement.DAL/Repositories/PantherTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace PantherPetManagement.DAL.Repositories
+{
+    public class PantherTypeRepository : IPantherTypeRepository
+    {
+        private readonly Su25pantherDbContext _context;
+        public PantherTypeRepository(Su25pantherDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<PantherType>> GetAllAsync()
+        {
+            return await _context.PantherTypes.ToListAsync();
+        }
+    }
+}
