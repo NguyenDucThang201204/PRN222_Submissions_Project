5a176a7e10ddd464f6e4b0f84a44fe44cc4e6693
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:14 +0700

    Upload solution/PantherPetManagement.DAL/Repositories/PantherAccountRepository.cs

---
+++ solution/PantherPetManagement.DAL/Repositories/PantherAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace PantherPetManagement.DAL.Repositories
+{
+    public class PantherAccountRepository : IPantherAccountRepository
+    {
+        private readonly Su25pantherDbContext _context;
+        public PantherAccountRepository(Su25pantherDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PantherAccount> Login(string email, string password)
+        {
+           return await _context.PantherAccounts.FirstOrDefaultAsync(x => x.Email == email && x.Password == password);
+        }
+    }
+}
