80379da5572a22c3fbd9142dffc60b15acc64e4a
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:37 +0700

    Upload solution/PantherPetManagement_TrinhHaiDuc/wwwroot/js/Reload.js

---
+++ solution/PantherPetManagement_TrinhHaiDuc/wwwroot/js/Reload.js
@@ -0,0 +1,8 @@
+﻿const connection = new signalR.HubConnectionBuilder().withUrl("/hub").build();
+connection.on("Reload", function (id) {
+    location.reload();
+});
+connection.start()
+    .then(() => console.log("SignalR connected"))
+    .catch(err => console.error(err));
+    //
