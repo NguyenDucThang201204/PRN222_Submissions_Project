2fd2fce0c2f847d395e0e1b5101633af953bec08
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:31 +0700

    Upload solution/DAL/IPantherTypeRepo.cs

---
+++ solution/DAL/IPantherTypeRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace DAL
+{
+    public interface IPantherTypeRepo
+    {
+        List<PantherType> GetPantherTypes();
+        PantherType GetPantherTypeById(int id);
+    }
+}
