719557c08f7ce3b2a91dbef231cf51f0276183a3
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:29:25 +0700

    Upload solution/PantherPetManagement_TranThienDuy/Pages/Panther/Details.cshtml.cs

---
+++ solution/PantherPetManagement_TranThienDuy/Pages/Panther/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+
+namespace PantherPetManagement_TranThienDuy.Pages.Panther
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Repositories.SU25PantherDBContext _context;
+
+        public DetailsModel(Repositories.SU25PantherDBContext context)
+        {
+            _context = context;
+        }
+
+        public PantherProfile PantherProfile { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pantherprofile = await _context.PantherProfiles.FirstOrDefaultAsync(m => m.PantherProfileId == id);
+            if (pantherprofile == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                PantherProfile = pantherprofile;
+            }
+            return Page();
+        }
+    }
+}
