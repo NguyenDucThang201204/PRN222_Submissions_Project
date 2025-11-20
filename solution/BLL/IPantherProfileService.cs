fa9b99fa38f51b02c486551672e6a691c8efa7e8
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:20 +0700

    Upload solution/BLL/IPantherProfileService.cs

---
+++ solution/BLL/IPantherProfileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace BLL
+{
+    public interface IPantherProfileService
+    {
+        void Save(PantherProfile pantherProfile);
+        void Delete(PantherProfile pantherProfile);
+        void Update(PantherProfile pantherProfile);
+        List<PantherProfile> GetAll();
+        PantherProfile GetById(int pantherProfileId);
+        List<PantherProfile> Search(double? weight, string pantherTypeName);
+    }
+}
