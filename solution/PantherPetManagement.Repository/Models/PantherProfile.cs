30b2d4458558a850172aa1149fd8c502f090cbb9
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:54 +0700

    Upload solution/PantherPetManagement.Repository/Models/PantherProfile.cs

---
+++ solution/PantherPetManagement.Repository/Models/PantherProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PantherPetManagement.Repository.Models;
+
+public partial class PantherProfile
+{
+    public int PantherProfileId { get; set; }
+
+    public int PantherTypeId { get; set; }
+
+    public string PantherName { get; set; } = null!;
+
+    public double Weight { get; set; }
+
+    public string Characteristics { get; set; } = null!;
+
+    public string Warning { get; set; } = null!;
+
+    public DateTime ModifiedDate { get; set; }
+
+    public virtual PantherType PantherType { get; set; } = null!;
+}
