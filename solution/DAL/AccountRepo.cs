c0b92701f4d190394ba2918260198cc3651baffc
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:28 +0700

    Upload solution/DAL/AccountRepo.cs

---
+++ solution/DAL/AccountRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace DAL
+{
+    public class AccountRepo : IAccountRepo
+    {
+        public PantherAccount? GetPantherAccount(string email, string password)
+        {
+            var _context = new Su25pantherDbContext();
+            return _context.PantherAccounts
+                .FirstOrDefault(a => a.Email == email && a.Password == password);
+        }
+    }
+}
