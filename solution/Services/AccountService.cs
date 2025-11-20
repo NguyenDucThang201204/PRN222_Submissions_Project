84f0308016cd5cb216b4ec93e41167780b8750ae
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:29:09 +0700

    Upload solution/Services/AccountService.cs

---
+++ solution/Services/AccountService.cs
@@ -0,0 +1,21 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AccountService
+    {
+        private readonly AccountRepo _loginRepo;
+
+        public AccountService(AccountRepo loginRepo)
+        {
+            _loginRepo = loginRepo;
+        }
+        public PantherAccount LoginAccount(string email, string password)
+        => _loginRepo.LoginAccount(email, password);
+    }
+}
