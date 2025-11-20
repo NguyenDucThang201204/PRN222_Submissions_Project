f350a6db02ce30e4d7cc5f2f9e0a2726d2469cd4
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:28:32 +0700

    Upload solution/DAL/PantherTypeRepo.cs

---
+++ solution/DAL/PantherTypeRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace DAL
+{
+    public class PantherTypeRepo : IPantherTypeRepo
+    {
+        public PantherType GetPantherTypeById(int id)
+        {
+            var context = new Su25pantherDbContext();
+            var cc = context.PantherTypes.Find(id);
+            context.Dispose();
+            return cc;
+        }
+
+        public List<PantherType> GetPantherTypes()
+        {
+            var context = new Su25pantherDbContext();
+            var list = context.PantherTypes.ToList();
+            context.Dispose();
+            return list;
+        }
+    }
+}
