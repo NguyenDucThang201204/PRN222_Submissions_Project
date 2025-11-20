4f5c98904361c6f269cb2edb8a7bc7fb676fbec6
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:10 +0700

    Upload solution/PantherPetManagement.BLL/Services/PantherProfileService.cs

---
+++ solution/PantherPetManagement.BLL/Services/PantherProfileService.cs
@@ -0,0 +1,41 @@
+﻿using PantherPetManagement.DAL;
+using PantherPetManagement.DAL.Repositories;
+
+
+namespace PantherPetManagement.BLL.Services
+{
+    public class PantherProfileService : IPantherProfileService
+    {
+        private readonly IPantherProfileRepository _repo;
+        public PantherProfileService(IPantherProfileRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<List<PantherProfile>> GetAllAsync()
+        {
+            return await _repo.GetAllAsync();
+        }
+        public async Task<PantherProfile> GetByIdAsync(int id)
+        {
+            return await _repo.GetByIdAsync(id);
+        }
+
+        public async Task<List<PantherProfile>> GetAllByOr(double? search1, string search2)
+        {
+            return await _repo.GetAllByOr(search1, search2);
+        }
+        public Task AddAsync(PantherProfile add)
+        {
+            return _repo.AddAsync(add);
+        }
+        public Task DeleteAsync(int id)
+        {
+            return _repo.DeleteAsync(id);
+        }
+        public Task UpdateASync(PantherProfile update)
+        {
+            return _repo.UpdateASync(update);
+        }
+
+    }
+}
