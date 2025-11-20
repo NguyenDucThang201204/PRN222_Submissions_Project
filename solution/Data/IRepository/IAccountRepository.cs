80286a86036ef7eeec60713c46596909655f0b62
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:26:24 +0700

    Upload solution/Data/IRepository/IAccountRepository.cs

---
+++ solution/Data/IRepository/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.IRepository
+{
+    public interface IAccountRepository
+    {
+        PantherAccount GetByEmail(string email);
+    }
+}
