37270bb4d310f67ad2513167424b137217a86271
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:43 +0700

    Upload solution/PantherPetManagement.Repository/PantherTypeRepo.cs

---
+++ solution/PantherPetManagement.Repository/PantherTypeRepo.cs
@@ -0,0 +1,27 @@
+﻿using PantherPetManagement.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantherPetManagement.Repository
+{
+    public class PantherTypeRepo : IPantherTypeRepo
+    {
+        public List<PantherType> GetCategories()
+        {
+            var listCategories = new List<PantherType>();
+            try
+            {
+                using var context = new Su25pantherDbContext();
+                listCategories = context.PantherTypes.ToList();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return listCategories;
+        }
+    }
+}
