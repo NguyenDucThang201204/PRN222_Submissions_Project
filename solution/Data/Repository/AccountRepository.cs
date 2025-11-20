f36db7954ae106ffd5614ae96256590c90aab0cb
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:26:29 +0700

    Upload solution/Data/Repository/AccountRepository.cs

---
+++ solution/Data/Repository/AccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using Data.IRepository;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly Su25pantherDbContext _context;
+
+        public AccountRepository(Su25pantherDbContext context)
+        {
+            _context = context;
+        }
+
+        public PantherAccount GetByEmail(string email)
+        {
+            return _context.PantherAccounts.FirstOrDefault(a => a.Email == email);
+        }
+    }
+}
