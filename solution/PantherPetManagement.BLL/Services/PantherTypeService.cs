c5ab359971e33583994a87d5f77ec5ef491f63a2
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:11 +0700

    Upload solution/PantherPetManagement.BLL/Services/PantherTypeService.cs

---
+++ solution/PantherPetManagement.BLL/Services/PantherTypeService.cs
@@ -0,0 +1,19 @@
+﻿using PantherPetManagement.DAL;
+using PantherPetManagement.DAL.Repositories;
+
+
+namespace PantherPetManagement.BLL.Services
+{
+    public class PantherTypeService : IPantherTypeService
+    {
+        private readonly IPantherTypeRepository _repo;
+        public PantherTypeService(IPantherTypeRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<List<PantherType>> GetAllAsync()
+        {
+           return await _repo.GetAllAsync();
+        }
+    }
+}
