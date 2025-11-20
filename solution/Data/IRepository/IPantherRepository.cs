8e0d6dd6e23471dd331d72fa4be4aba70ed041ef
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:26:25 +0700

    Upload solution/Data/IRepository/IPantherRepository.cs

---
+++ solution/Data/IRepository/IPantherRepository.cs
@@ -0,0 +1,20 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.IRepository
+{
+    public interface IPantherRepository
+    {
+        IQueryable<PantherProfile> GetAll();
+        PantherProfile GetById(int id);
+        void Add(PantherProfile pantherProfile);
+        void Update(PantherProfile pantherProfile);
+        void Delete(int id);
+        IQueryable<PantherType> GetPantherTypes();
+        int GetTotalCount();
+    }
+}
