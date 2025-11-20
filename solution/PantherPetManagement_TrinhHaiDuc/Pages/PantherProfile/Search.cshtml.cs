e05846cb0efe4e192debb5879d4bc805804bec27
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:31 +0700

    Upload solution/PantherPetManagement_TrinhHaiDuc/Pages/PantherProfile/Search.cshtml.cs

---
+++ solution/PantherPetManagement_TrinhHaiDuc/Pages/PantherProfile/Search.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PantherPetManagement.BLL.Services;
+
+namespace PantherPetManagement_TrinhHaiDuc.Pages.PantherProfile
+{
+    [Authorize(Policy = "View")]
+    public class SearchModel : PageModel
+    {
+        private readonly IPantherProfileService _service;
+        public SearchModel(IPantherProfileService service)
+        {
+            _service = service;
+        }
+
+        public string Role { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 3;
+        public int TotalPage { get; set; }
+        public List<PantherPetManagement.DAL.PantherProfile> Entities { get; set; } = new();
+
+        [BindProperty(SupportsGet = true)]
+        public double? Search1 { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Search2 { get; set; }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var all = await _service.GetAllByOr(Search1, Search2);
+            TotalPage = (int)Math.Ceiling((double)all.Count / PageSize);
+            PageIndex = Math.Max(1, Math.Min(PageIndex, TotalPage));
+            Entities = all
+                .Skip((PageIndex - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+            return Page();
+        }
+    }
+}
