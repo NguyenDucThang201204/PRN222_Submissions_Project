1b451b6a1092f2bcb908ccb967c2f181bd7f6f46
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:06 +0700

    Upload solution/PantherPetManagement.BLL/Services/IPantherProfileService.cs

---
+++ solution/PantherPetManagement.BLL/Services/IPantherProfileService.cs
@@ -0,0 +1,15 @@
+﻿using PantherPetManagement.DAL;
+
+
+namespace PantherPetManagement.BLL.Services
+{
+    public interface IPantherProfileService
+    {
+        Task<List<PantherProfile>> GetAllAsync();
+        Task<List<PantherProfile>> GetAllByOr(double? search1, string search2);
+        Task AddAsync(PantherProfile add);
+        Task DeleteAsync(int id);
+        Task UpdateASync(PantherProfile update);
+        Task<PantherProfile> GetByIdAsync(int id);
+    }
+}
