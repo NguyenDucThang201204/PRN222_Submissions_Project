66cc86685227f43fcddf7d81d0c3e7729d7e7e1c
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:27:49 +0700

    Upload solution/PantherPetManagement.Service/PantherTypeService.cs

---
+++ solution/PantherPetManagement.Service/PantherTypeService.cs
@@ -0,0 +1,23 @@
+﻿using PantherPetManagement.Repository;
+using PantherPetManagement.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantherPetManagement.Service
+{
+    public class PantherTypeService : IPantherTypeService
+    {
+        private readonly IPantherTypeRepo _repo;
+        public PantherTypeService(IPantherTypeRepo repo)
+        {
+            _repo = repo;
+        }
+        public List<PantherType> GetCategories()
+        {
+            return _repo.GetCategories();
+        }
+    }
+}
