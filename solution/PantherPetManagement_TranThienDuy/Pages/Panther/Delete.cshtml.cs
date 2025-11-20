c007a74515317a5989b2a98286dc2c5a8ae54e80
Nguyen Thang <thangnguyenduc2004@gmail.com>
2025-11-20 14:29:23 +0700

    Upload solution/PantherPetManagement_TranThienDuy/Pages/Panther/Delete.cshtml.cs

---
+++ solution/PantherPetManagement_TranThienDuy/Pages/Panther/Delete.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Services;
+
+namespace PantherPetManagement_TranThienDuy.Pages.Panther
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly PantherProfileService _context;
+
+        public DeleteModel(PantherProfileService context)
+        {
+            _context = context;
+        }
+
+        public int roleID { get; set; }
+
+        [BindProperty]
+        public PantherProfile PantherProfile { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            roleID = HttpContext.Session.GetInt32("RoleId") ?? 0;
+            if (roleID != null && roleID != 2)
+            {
+                return RedirectToPage("/AccessDenied");
+
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pantherprofile = _context.GetById(id);
+
+            if (pantherprofile == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                PantherProfile = pantherprofile;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pantherprofile = _context.GetById(id);
+            if (pantherprofile != null)
+            {
+                PantherProfile = pantherprofile;
+                _context.Delete(id);
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
